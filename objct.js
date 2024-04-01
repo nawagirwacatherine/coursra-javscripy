d7597f3adbaf83e8a033165d113f82a3030f55c7
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-04-01 14:17:45 +0300

    counted life in weeks of remaining for a 30year old

---
+++ objct.js
@@ -0,0 +1,15 @@
+var hotel = {}
+
+hotel.name = 'The Grand';
+hotel.price = 200;
+hotel.stars = 5;
+hotel.isFull = false;
+
+
+console.log(hotel)
+
+hotel.summariseHptel = function(){
+    console.log('The Grand is a beautiful5 star hotel');
+}
+
+hotel.summariseHotel()

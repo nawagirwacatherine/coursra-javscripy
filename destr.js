fd6ca3ea4762e783573ab4b0fc8f787061e47b7b
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-05-20 12:18:21 +0200

    used brackets notation

--- destr.js
+++ destr.js
@@ -1,10 +1,28 @@
-var clothingItem = {
-    price: 50,
-    color: 'beige',
-    material: 'cotton',
-    season:'autumn'
-}
+// var clothingItem = {
+//     price: 50,
+//     color: 'beige',
+//     material: 'cotton',
+//     season:'autumn'
+// }
 
-for (const key of Object.keys(clothingItem)){
-    console.log(key,":",clothingItem[key])
+// for (const key of Object.keys(clothingItem)){
+//     console.log(key,":",clothingItem[key])
+// }
+
+function testBracketsDynamicAccess(){
+    var dynamicKey;
+    if(Math.random() > 0.5) {
+        dynamicKey = "speed";
+    }
+    else{
+        dynamicKey ="color";
+    }
+
+    var drone = {
+        speed: 15,
+        color: "orange"
+    }
+
+    console.log(drone[dynamicKey]);
 }
+testBracketsDynamicAccess();
